4b64b46889b911aa4cb2858cf97381bf96683cd6
Yordi Ogando <yordi.ogando@pitech.com.do>
2020-06-20 00:27:10 -0400

    Updates

---
+++ precache-manifest.45f9bf8ab57c1817344a9fa7afb82b8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab90310b0148be84352bf21b80f7cd4f",
+    "url": "/monsters_rolodex/index.html"
+  },
+  {
+    "revision": "f8e06ce54d818eea162d",
+    "url": "/monsters_rolodex/static/css/main.2b70a2a9.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monsters_rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters_rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8e06ce54d818eea162d",
+    "url": "/monsters_rolodex/static/js/main.91ad8354.chunk.js"
+  },
+  {
+    "revision": "e7234e844f89cf2f131c",
+    "url": "/monsters_rolodex/static/js/runtime-main.fc94bd7d.js"
+  }
+]);
