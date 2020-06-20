46f8c7e80eb89a32fe5dee0c33385dea97bb8964
Yordi Ogando <yordi.ogando@pitech.com.do>
2020-06-20 00:11:13 -0400

    Creating searchbox component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { CardList } from "./components/card-list/card-list.component.jsx";
+import { SearchBox } from "./components/search-box/search-box.component";
+
 import "./App.css";
 
 class App extends Component {
@@ -10,6 +12,11 @@ class App extends Component {
       monsters: [],
       searchField: "",
     };
+
+    //This is for defining the context of "this" inside of the handleChange function
+    //to the App class insted of the function (in case we use normal functions instead of arrow functions)
+
+    //this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -17,6 +24,10 @@ class App extends Component {
       .then((res) => res.json())
       .then((users) => this.setState({ monsters: users }));
   }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
 
   render() {
     const { monsters, searchField } = this.state;
@@ -26,10 +37,9 @@ class App extends Component {
 
     return (
       <div className="App">
-        <input
-          type="search"
-          placeholder="search monster"
-          onChange={(e) => this.setState({ searchField: e.target.value })}
+        <SearchBox
+          placeholder={"search monsters"}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters} />
       </div>
